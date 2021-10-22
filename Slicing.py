6525c753786b7ef64b94024809520dd9986468c8
Suraj Saurabh <surajsaurabh@ymail.com>
2021-10-22 23:41:54 +0530

    Slicing

---
+++ Slicing.py
@@ -0,0 +1,13 @@
+z ="Royal Challengers Bangalore"
+
+# Challengers
+print(z[6:17])
+
+# aglr from Bangalore
+print(z[19:26:2])
+
+# Royal in reverse
+print(z[-23:-28:-1])
+
+# lengers from Challengers
+print(z[-17:-10])
