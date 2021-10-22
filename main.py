51316f364fb38844534678f6c2fb5304b3d7308c
Suraj Saurabh <surajsaurabh@ymail.com>
2021-10-22 22:33:22 +0530

    Tuple

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 s = ([1,9,2,8,7,"pyspiders"],12,(47.8,6-5j),((["hello"])))
+e = ((["hello","bye"]),(['bengaluru']),("chennai"),11+3j,(317),(25,74))
 
 # 2nd l of hello
 print(s[3][0][3])
@@ -11,3 +12,12 @@ print(s[0][5][5])
 
 # 1 from the first list
 print(s[0][0])
+
+# string of chennai
+print(e[2])
+
+# 74 of tuple (25,74)
+print(e[5][1])
+
+# bye from list
+print(e[0][1])
