e4ab620e8eb26f07487b50f2badedc95b8085af1
Suraj Saurabh <surajsaurabh@ymail.com>
2021-10-23 19:19:45 +0530

    Type Casting Float,Complex

--- Type Casting.py
+++ Type Casting.py
@@ -18,3 +18,47 @@ print(int("123"))
 print(int("67"))
 
 # float into int,bool,string(only numbers)
+
+# int
+print("\nInteger")
+print(float(45))
+print(float(12))
+
+# bool
+print("\nBoolean")
+print(float(True))
+print(float(False))
+
+# str
+print("\nString")
+print(float("24"))
+print(float("32.43"))
+print(float("89.02"))
+
+# ComplexNumber into int,float,boolean,str(a+-bj)
+
+# int
+print("\nIntger")
+print(complex(4))
+print(complex(56,-18))
+
+# float
+print("\nFloat")
+print(complex(23.21))
+print(complex(-43.34,29.51))
+
+#boolean
+print("\nBoolean")
+print(complex(True, False))
+print(complex(False, True))
+print(complex(True, True))
+print(complex(False, False))
+
+# str
+print("\nString")
+print(complex("61.12-73j"))
+print(complex("92.0+34j"))
+
+# Bool
+print("\nIntger")
+print(bool())
