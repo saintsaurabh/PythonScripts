cc487852df93fb359ba5b7fae71396cb3bd08ce4
Suraj Saurabh <surajsaurabh@ymail.com>
2021-10-23 11:05:57 +0530

    More Slicing

--- Slicing.py
+++ Slicing.py
@@ -1,4 +1,6 @@
 z ="Royal Challengers Bangalore"
+g =[1,2,("bengaluru",["chennai"]),(("mumbai"),("kolkata"))]
+b ={'a':['Guwahati'],('hello'):('world'),11:[['hello world'],(1,2,3,'ajanta')]}
 
 # Challengers
 print(z[6:17])
@@ -11,3 +13,15 @@ print(z[-23:-28:-1])
 
 # lengers from Challengers
 print(z[-17:-10])
+
+# nnai from chennai
+print(g[2][1][0][3:7])
+
+# kka from kolkata
+print(g[3][1][::3])
+
+# mumbai in reverse
+print(g[3][0][-1:-7:-1])
+
+# bengal from bengaluru
+print(g[2][0][:6:])
