109f9033efab60d1fe9c4d33e8d0c5c58612658c
Suraj Saurabh <surajsaurabh@ymail.com>
2021-10-23 14:08:35 +0530

    Type Casting int

---
+++ Type Casting.py
@@ -0,0 +1,20 @@
+#  int into float,boolean,string(only numbers)
+
+# float
+print("Float")
+print(int(2.36))
+print(int(78.34))
+print(int(44.3))
+
+# boolean
+print("\nBoolean")
+print(int(True))
+print(int(False))
+
+# String(only number)
+print("\nString")
+print(int("56"))
+print(int("123"))
+print(int("67"))
+
+# float into int,bool,string(only numbers)
